aadadf177a61487b3b95a67511bb95d235d431d4
kiraarghy <karaestubbs@gmail.com>
2019-12-06 15:56:34 +0000

    Some header work done

--- src/partials/header.js
+++ src/partials/header.js
@@ -1,54 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Link, Prefetch } from "react-static";
-// import SVGInline from "react-svg-inline"
-// I don't love this solution but I've been having major trubs trying to get
-// the base64 encoded string to convert to an svg, attempted using both
-// raw-loader and svg-loader as well as raw html and a few svg react libs.
-// Possibly something further upstream from the base react-static config,
-// but this solution is working, which is always a nice feature to have.
+import styled from "styled-components";
 import SVG from "react-inlinesvg";
 
-// Common
-import Header from "../../src/formidable-landers/src/components/header";
+// ommon
 import config from "../../static-config-parts/site-data";
-import svgHeroLogo from "../../static/logotype-hero.svg";
+import Logo from "../formidable-landers/src/assets/logo.svg";
 
-class VictoryHeader extends Component {
-  render() {
-    const victoryLogo = (
-      <Link
-        to="/"
-        style={{
-          display: "block",
-          height: "50px"
-        }}
-      >
-        {/* eslint-disable-next-line react/jsx-pascal-case */}
-        <SVG style={{ height: "30px" }} src={svgHeroLogo} onLoad={src => src} />
-      </Link>
-    );
-
-    return (
-      <Header className="victory" theme="light">
-        <div className="default" style={{ paddingBottom: 0 }}>
-          {/* <SVGInline svg={ LOGO }/> */}
-          {victoryLogo}
-          <Link to="/about/">About</Link>
-          <Link to="/docs/">Docs</Link>
-          <Prefetch path="/gallery">
-            <Link to="/gallery/">Gallery</Link>
-          </Prefetch>
-          {config.projectLinks.map(link => (
-            <a key={link.url} href={link.url}>
-              {link.label}
-            </a>
-          ))}
-        </div>
-      </Header>
-    );
-  }
-}
+const VictoryHeader = () => {
+  return (
+    <Header>
+      <VictoryHeading>Victory</VictoryHeading>
+      <LinkContainer>
+        <Link to="/about/">About</Link>
+        <Link to="/docs/">Docs</Link>
+        <Prefetch path="/gallery">
+          <Link to="/gallery/">Gallery</Link>
+        </Prefetch>
+        {config.projectLinks.map(link => (
+          <a key={link.url} href={link.url}>
+            {link.label}
+          </a>
+        ))}
+      </LinkContainer>
+      <FormidableLogo src={Logo} />
+    </Header>
+  );
+};
 
 VictoryHeader.propTypes = {
   home: PropTypes.bool
@@ -59,3 +38,31 @@ VictoryHeader.defaultProps = {
 };
 
 export default VictoryHeader;
+
+const Header = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  box-shadow: 0px 2px 7px 0px rgba(0, 0, 0, 0.14);
+  @media (min-width: 760px) {
+    padding: 0 ${props => props.theme.gutter};
+  }
+`;
+
+const VictoryHeading = styled.h1`
+  margin: ${props => props.theme.gutterSmall} 0;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  > a {
+    margin-left: ${props => props.theme.gutterSmall};
+    text-transform: uppercase;
+  }
+`;
+
+const FormidableLogo = styled(SVG)`
+  width: 9rem;
+  height: 2rem;
+  margin-left: auto;
+`;
