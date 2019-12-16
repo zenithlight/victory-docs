c4c6d7098a76fc2d6f3717b1911fdef7bffe3bbb
kiraarghy <karaestubbs@gmail.com>
2019-12-16 16:26:18 +0000

    task/rewrite
    Fix failing lint error.

--- src/formidable-landers/src/index.js
+++ src/formidable-landers/src/index.js
@@ -1,11 +1,9 @@
-import Footer from "./components/footer";
 import CookieBanner from "./components/cookie-banner";
 import VictorySettings from "./themes/victory-settings";
 import appendLinkIcon from "./markdown-renderers/append-link-icon";
 import ecologyPlaygroundLoading from "./markdown-renderers/ecology-playground-loading";
 
 export {
-  Footer,
   CookieBanner,
   VictorySettings,
   appendLinkIcon,
